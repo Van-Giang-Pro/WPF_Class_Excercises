52780eeeeeb0e8126692938b52783b92c38a79af
Giang <vangiang260594@gmail.com>
2026-06-11 16:44:43 -0400

    Modify view models

--- Navigation Basic/Commands/UpdateCurrentViewModel.cs
+++ Navigation Basic/Commands/UpdateCurrentViewModel.cs
@@ -6,15 +6,12 @@ using System.Windows.Input;
 
 namespace Navigation_Basic.Commands
 {
-    public class UpdateCurrentViewModel : ICommand
+    public class UpdateCurrentViewModel : ICommand // Đây không phải là kế thừa nha
     {
         public event EventHandler? CanExecuteChanged;
-
         public bool CanExecute(object? parameter) => true;
-
         public void Execute(object? parameter)
         {
-            throw new NotImplementedException();
         }
     }
 }
--- Navigation Basic/ViewModels/MainWindowViewModel.cs
+++ Navigation Basic/ViewModels/MainWindowViewModel.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace Navigation_Basic.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
         private ViewModelBase _currentViewModel;
-
         public ViewModelBase CurrentViewModel
         {
             get { return _currentViewModel; }
-            set { _currentViewModel = value; }
+            set
+            {
+                _currentViewModel = value;
+                OnPropertyChanged();
+            }
         }
+        public ICommand UpdateCurrentViewModel { get; }
     }   
 }
