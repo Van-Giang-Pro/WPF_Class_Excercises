c496e0855612baea93012980e819e31df314f7e7
Giang <vangiang260594@gmail.com>
2026-06-12 16:57:04 -0400

    Create new project navigation with DI and FP

---
+++ Navigation With Dependency Injection And Factory Pattern/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Navigation_With_Dependency_Injection_And_Factory_Pattern;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
