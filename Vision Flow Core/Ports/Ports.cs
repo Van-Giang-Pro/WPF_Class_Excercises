e1df9430feb84c08de496caa2795d80d68340c81
Giang <vangiang260594@gmail.com>
2026-06-26 19:15:52 -0400

    Working on Ports.cs

--- Vision Flow Core/Ports/Ports.cs
+++ Vision Flow Core/Ports/Ports.cs
@@ -14,3 +14,36 @@ public interface IPort
     PortDirection Direction { get; }
     object? Value { get; set; }
 }
+
+public interface IInputPort : IPort
+{
+    bool IsOptional { get; }
+}
+
+public interface IOutphutPort : IPort
+{
+    
+}
+
+public sealed class InputPort<T> : IInputPort
+{
+    public InputPort(string name, string? displayName = null, bool isOptional = false)
+    {
+        Name = name;
+        Displayname = displayName ?? name;
+        IsOptional = isOptional;
+    }
+    
+    public string Name { get; }
+    public string Displayname { get; }
+    public Type DataType => typeof(T);
+    public PortDirection Direction => PortDirection.Input;
+    public bool IsOptional { get; }
+    public T? Value { get; set; }
+
+    object? IPort.Value
+    {
+        get => Value;
+        set => Value = value is null ? default : (T)value;
+    }
+}
