54bde684dc45cf3676d76e741de69571afdcdc9e
Giang <vangiang260694@gmail.com>
2026-07-08 06:44:41 -0400

    Implemented the test program

--- Test Vision Flow Function/Program.cs
+++ Test Vision Flow Function/Program.cs
@@ -35,7 +35,34 @@ public class Program
             Console.WriteLine("Không thể load ảnh : " + imagePath);
             return;
         }
-        Console.WriteLine("Đã load ảnh : " + imagePath);
+        Console.WriteLine($"Đã load ảnh : {src.Width}x{src.Height}");
+
+        var tool = new FindLineTool();
+        tool.FindInput("Image")!.Value = new MatVisionImage(src);
+
+        tool.FindParameter("Number Of Caliper")!.Value = 15;
+        tool.FindParameter("Edge Threshold")!.Value = 4.0;
+        tool.FindParameter("Edge Polarity")!.Value = Either;
+
+        var context = new SimpleToolContext();
+        tool.Execute(context);
+
+        var overLayImg = (IVisionImage)tool.FindOutput("Line")!.Value;
+        var line = (LineResult)tool.FindOutput("Line")!.Value;
+        var edges = (P2[])tool.FindOutput("EdgePoints")!.Value;
+        var rms = (double)tool.FindOutput("RMSError")!.Value;
+        var score = (double)tool.FindOutput("Score")!.Value;
+        
+        Console.WriteLine($"Judge={line.Judge} Angle=(line.AngleDeg:f2} rms={rms:F2} Score={score:F2} Edges={edges.Length}");
+
+        Console.Imwrite(outputPath, overLayImg.AsMat());
+        Console.WriteLine("Đã lưu : " + outputPath);
+        
+        overLayImg.Dispose();
+        src.Dispose();
+
+        Cv2.WaitKey();
+        Cv2.DestroyAllWindows();
     }
 }
 
