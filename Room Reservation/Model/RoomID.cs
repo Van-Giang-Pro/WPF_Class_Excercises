120a8e8f81244d26ce7629f286f82d0c3c77b591
Giang <vangiang260694@gmail.com>
2026-07-20 06:53:52 -0400

    Add comments to RoomID.cs

--- Room Reservation/Model/RoomID.cs
+++ Room Reservation/Model/RoomID.cs
@@ -31,5 +31,8 @@ namespace Room_Reservation.Model
         {
             return HashCode.Combine(FloorNumber, RoomNumber);
         }
+        // Trộn 2 số vào để cho ra 1 số nếu floor và room giống nhau thì sẽ cho ra cùng 1 số
+        // Phòng A và Phòng B tuy cùng là 301 tầng 3 nhưng được tạo ở 2 chỗ khác nhau trong bộ nhớ, máy tính coi là 2 phòng khác nhau ❌ (sai về mặt logic)
+        // Khi override, bạn bảo máy tính đừng nhìn bộ nhớ, hãy nhìn vào số tầng và số phòng thì A và B được coi là cùng một phòng
     }
 }
